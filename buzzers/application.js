112cfdd8fafa4f9a25337c4301eea4b47a90fec8
Robert Levelind <robert.levelind@tink.com>
2024-05-02 16:49:24 +0200

    Update application.js

--- buzzers/application.js
+++ buzzers/application.js
@@ -17,17 +17,18 @@ const pollGamepads = () => {
   // Always call `navigator.getGamepads()` inside of
   // the game loop, not outside.
   const gamepads = navigator.getGamepads();
-	
   for (const gamepad of gamepads) {
-	  
     // Disregard empty slots.
     if (!gamepad) {
       continue;
     }
-	  console.log(gamepad.buttons[0].pressed)
-	  console.log(gamepad)
-		  window.requestAnimationFrame(pollGamepads);
-}};
+    // Process the gamepad state.
+    console.log(gamepad);
+  }
+  // Call yourself upon the next animation frame.
+  // (Typically this happens every 60 times per second.)
+  window.requestAnimationFrame(pollGamepads);
+};
 // Kick off the initial game loop iteration.
 pollGamepads();
 
