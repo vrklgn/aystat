6e7a5c023f4acf90dc8a1616dee6a6a710e2bbe2
Robert Levelind <robert.levelind@tink.com>
2024-05-02 17:09:08 +0200

    Update application.js

--- buzzers/application.js
+++ buzzers/application.js
@@ -298,7 +298,6 @@ const pollGamepads = () => {
       continue;
     }
 	  	if(gamepad.buttons[0].pressed == true){
-			if(!JP.answersAccepted || playerIsAnswering) return false;
 		playerThatIsAnswering = 1; 
 		console.log ("PLAYER 1");
 	// If player has already answered, return false
